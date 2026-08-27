9d8ca2fde249c59c4895ca40f1c6ccc9fecb7d78
weicong <weicong@live.cn>
2026-08-27 16:23:00 +0800

    feat[swagger]: 优化Swagger文档生成与枚举类型展示
    在公共配置中开启XML文档生成，并在Swagger配置中增加对接口摘要注释的动态加载。新增SwaggerEnumSchemaFilter以在Schema中补充枚举名称信息，同时优化泛型类型的SchemaId生成策略，避免文档生成冲突。
    
    VisionNO: US2026010100001

--- main/AcroStack/Swagger/SwaggerConfigurationExtensions.cs
+++ main/AcroStack/Swagger/SwaggerConfigurationExtensions.cs
@@ -18,13 +18,43 @@ public static class SwaggerConfigurationExtensions
             {
                 options.SwaggerDoc("v1", new OpenApiInfo { Title = "AcroStack API", Version = "v1" });
                 options.DocInclusionPredicate((docName, description) => true);
-                options.CustomSchemaIds(type => type.FullName);
+                options.CustomSchemaIds(SchemaIdSelector);
                 options.CustomOperationIds(apiDesc =>
                 {
                     var controller = apiDesc.ActionDescriptor.RouteValues["controller"];
                     var action = apiDesc.ActionDescriptor.RouteValues["action"];
                     return $"{controller}_{action}";
                 });
+
+                options.SchemaFilter<SwaggerEnumSchemaFilter>();
+
+                if (configuration.GetValue<bool>("Swagger:ShowSummaries"))
+                {
+                    var loadedAssemblyNames = AppDomain.CurrentDomain.GetAssemblies()
+                        .Where(a => !a.IsDynamic && !string.IsNullOrEmpty(a.GetName().Name))
+                        .Select(a => a.GetName().Name + ".xml")
+                        .ToHashSet();
+
+                    foreach (var xmlFile in Directory.GetFiles(AppContext.BaseDirectory, "*.xml"))
+                    {
+                        if (loadedAssemblyNames.Contains(Path.GetFileName(xmlFile)))
+                        {
+                            options.IncludeXmlComments(xmlFile);
+                        }
+                    }
+                }
             });
+    }
+
+    private static string SchemaIdSelector(Type modelType)
+    {
+        if (!modelType.IsConstructedGenericType)
+        {
+            return modelType.FullName;
+        }
+
+        var name = modelType.Name.Split('`').First();
+        var args = string.Join("", modelType.GetGenericArguments().Select(SchemaIdSelector));
+        return name + "Of" + args;
     }
 }
---
+++ main/AcroStack/Swagger/SwaggerEnumSchemaFilter.cs
@@ -0,0 +1,25 @@
+using System.Text.Json.Nodes;
+using Microsoft.OpenApi;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace AcroStack.Swagger;
+
+public class SwaggerEnumSchemaFilter : ISchemaFilter
+{
+    public void Apply(IOpenApiSchema schema, SchemaFilterContext context)
+    {
+        var type = context.Type;
+        if (type is null || !type.IsEnum || schema.Extensions.ContainsKey("x-enumNames"))
+        {
+            return;
+        }
+
+        var enumNames = new JsonArray();
+        foreach (var name in Enum.GetNames(type))
+        {
+            enumNames.Add(name);
+        }
+
+        schema.Extensions.Add("x-enumNames", new JsonNodeExtension(enumNames));
+    }
+}
